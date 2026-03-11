13fc2afa4c9f5ecec1216efc6e46e30ee19d30d0
[dyad] <sandervdmolen2010@gmail.com>
2026-03-11 14:04:42 +0100

    [dyad] Fixed import error by removing non-existent terminal component - wrote 1 file(s)

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,1 +1,3 @@
-export { default as Terminal } from "./terminal"; export { default as LucideTerminal } from "./terminal"; // Fixed syntax
+export { default as ArrowDown } from "./arrow-down";
+export { default as ArrowUp } from "./arrow-up";
+export { default as Activity } from "./activity";
