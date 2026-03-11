09700e1f1cdc6484376b45c6e7e408d592c854a0
[dyad] <sandervdmolen2010@gmail.com>
2026-03-11 14:04:06 +0100

    [dyad] Fixed syntax error in UI component barrel index - wrote 1 file(s)

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,1 @@
-export { default as ArrowDown } from "./arrow-down";
-export { default as ArrowUp } from "./arrow-up";
-export { default as Activity } from "./activity";
-export { default as Terminal } from "./terminal";
-export { default as LucideTerminal } from "./terminal"; // Fixed syntax
+export { default as Terminal } from "./terminal"; export { default as LucideTerminal } from "./terminal"; // Fixed syntax
