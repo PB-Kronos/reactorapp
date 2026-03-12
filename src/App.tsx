6a80076964d23858d2f1dfa9bfe8f6bd507176db
[dyad] <sandervdmolen2010@gmail.com>
2026-03-12 13:14:11 +0100

    Reverted all changes back to version b1cb89fd4963f0202b17f16dc1a83f9cce01f756

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const queryClient = new QueryClient();
-<SpeedInsights/>
+
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
